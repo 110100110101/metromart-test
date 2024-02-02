0c00d707dc2494c26bf6e286a59793988e93b745
Jaja Yting <jaja@yting.dev>
2024-02-02 17:28:54 +0800

    Declare User model

---
+++ MetroMart-App/MetroMart/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  MetroMart-App
+//
+//  Created by Jaja Yting on 2/2/24.
+//
+
+import Foundation
+
+struct User {
+    
+    var username: String
+    var avatar: URL
+    var profile: URL
+}
