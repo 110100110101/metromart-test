e018dc49b2ee639455f5a6c56a3565563fb1d189
Jaja Yting <jaja@yting.dev>
2024-02-02 16:35:50 +0800

    Initial Commit

---
+++ MetroMart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetroMart
+//
+//  Created by Jaja Yting on 2/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
