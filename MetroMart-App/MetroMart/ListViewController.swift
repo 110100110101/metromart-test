bb512df15fc8e6117ce4035d325266993ae63057
Jaja Yting <jaja@yting.dev>
2024-02-02 18:13:20 +0800

    Revert to old method of setting root view controller in app delegate

--- MetroMart-App/MetroMart/ListViewController.swift
+++ MetroMart-App/MetroMart/ListViewController.swift
@@ -9,4 +9,18 @@ import UIKit
 
 class ListViewController: UIViewController {
     
+    private let viewModel: ListViewModel
+    
+    convenience init() {
+        fatalError("Use the designated initializer init(viewModel:)!")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented, so use init(viewModel:)!")
+    }
+    
+    init(viewModel: ListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
 }
