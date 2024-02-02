0a9414f000bca5263b3deaa5b2a006f06218233b
Jaja Yting <jaja@yting.dev>
2024-02-02 17:44:47 +0800

    Declare the functionalities of the ListViewModel

--- MetroMart-App/MetroMart/ListViewModel.swift
+++ MetroMart-App/MetroMart/ListViewModel.swift
@@ -6,7 +6,38 @@
 //
 
 import Foundation
+import RxRelay
+
+protocol UserProvider {
+    
+    func getUsers(completion: @escaping ([User]?) -> ())
+}
 
 class ListViewModel {
     
+    private let isLoading = PublishRelay<Bool>()
+    private let isErrorEncountered = PublishRelay<Bool>()
+    private let users = PublishRelay<[User]>()
+    
+    private let provider: UserProvider
+    
+    init(provider: UserProvider) {
+        self.provider = provider
+    }
+    
+    func getUsers() {
+        
+        self.isLoading.accept(true)
+        
+        self.provider.getUsers { [weak self] fetchedUsers in
+            
+            guard let fetchedUsers = fetchedUsers else {
+                self?.isLoading.accept(false)
+                self?.isErrorEncountered.accept(true)
+                return
+            }
+            
+            self?.users.accept(fetchedUsers)
+        }
+    }
 }
