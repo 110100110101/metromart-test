2a7af2a126a23a695d06a5138c094fbe816b573d
Jaja Yting <jaja@yting.dev>
2024-02-02 18:05:06 +0800

    Make User model decodable

--- MetroMart-App/MetroMart/User.swift
+++ MetroMart-App/MetroMart/User.swift
@@ -13,3 +13,42 @@ struct User {
     var avatar: URL
     var profile: URL
 }
+
+extension User: Decodable {
+    
+    private enum CodingKeys: String, CodingKey {
+        
+        case username = "login"
+        case avatar = "avatar_url"
+        case profile = "html_url"
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // Username
+        
+        self.username = try container.decode(String.self, forKey: .username)
+        
+        // Avatar
+        
+        if let parsedAvatarReadableURL = try? container.decode(String.self, forKey: .avatar),
+           let parsedAvatarURL = URL(string: parsedAvatarReadableURL) {
+            self.avatar = parsedAvatarURL
+        }
+        else {
+            throw DecodingError.dataCorrupted(.init(codingPath: [CodingKeys.avatar], debugDescription: "Corrupted avatar URL"))
+        }
+        
+        // Profile
+        
+        if let parsedProfileReadableURL = try? container.decode(String.self, forKey: .profile),
+           let parsedProfileURL = URL(string: parsedProfileReadableURL) {
+            self.profile = parsedProfileURL
+        }
+        else {
+            throw DecodingError.dataCorrupted(.init(codingPath: [CodingKeys.avatar], debugDescription: "Corrupted profiles URL"))
+        }
+    }
+}
