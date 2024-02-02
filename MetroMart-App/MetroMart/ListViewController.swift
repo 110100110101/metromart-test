77df1b6f80a19d61589ff716bd0d697c145e7367
Jaja Yting <jaja@yting.dev>
2024-02-02 18:41:58 +0800

    Initialize the tableView in the list view controller

--- MetroMart-App/MetroMart/ListViewController.swift
+++ MetroMart-App/MetroMart/ListViewController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDataSource {
+    
+    private var tableView: UITableView!
     
     private let viewModel: ListViewModel
+    
+    // MARK: - Initializers
     
     convenience init() {
         fatalError("Use the designated initializer init(viewModel:)!")
@@ -22,5 +26,42 @@ class ListViewController: UIViewController {
     init(viewModel: ListViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+    }
+    
+    // MARK: View Lifecycles
+    
+    override func loadView() {
+        
+        let view = UIView()
+        view.backgroundColor = .orange
+        
+        self.tableView = UITableView()
+        self.tableView.register(UserTableViewCell.self, forCellReuseIdentifier: "UserTableViewCell")
+        self.tableView.estimatedRowHeight = 32.0
+        self.tableView.rowHeight = UITableView.automaticDimension
+        
+        view.addSubview(self.tableView)
+        self.tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        self.view = view
+    }
+    
+    // MARK: - UITableViewDataSource Methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewModel.users.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: "UserTableViewCell", for: indexPath) as! UserTableViewCell
+        
+        return dequeuedCell
     }
 }
