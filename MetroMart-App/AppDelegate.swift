6085ff8666def857e5cf0df5f56c5ef7b3f4c7c9
Jaja Yting <jaja@yting.dev>
2024-02-02 18:15:29 +0800

    Initialize ListViewControllers, embed it in the navigation controller and present it

--- MetroMart-App/AppDelegate.swift
+++ MetroMart-App/AppDelegate.swift
@@ -13,6 +13,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let frame = UIScreen.main.bounds
+        self.window = UIWindow(frame: frame)
+        
+        let concreteProvider = ConcreteProvider()
+        let listViewModel = ListViewModel(provider: concreteProvider)
+        let listViewController = ListViewController(viewModel: listViewModel)
+        let navigationController = UINavigationController(rootViewController: listViewController)
+        
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 }
