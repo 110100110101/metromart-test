6e15250fcab0f5be5184b5abc8e8c52d8cad47ce
Jaja Yting <jaja@yting.dev>
2024-02-02 17:47:20 +0800

    Declare a concrete implementation of UserProvider

---
+++ MetroMart-App/MetroMart/ConcreteUseProvider.swift
@@ -0,0 +1,15 @@
+//
+//  ConcreteUseProvider.swift
+//  MetroMart-App
+//
+//  Created by Jaja Yting on 2/2/24.
+//
+
+import Foundation
+
+class ConcreteProvider: UserProvider {
+    
+    func getUsers(completion: @escaping ([User]?) -> ()) {
+        // TODO: Use Moya
+    }
+}
